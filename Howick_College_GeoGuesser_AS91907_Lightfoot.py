be0f7b4147844906622a66f77dc3d8a780a836ec
EvanLightfoot <168693883+EvanLightfoot@users.noreply.github.com>
2024-06-24 10:10:49 +1200

    Add files via upload

--- Howick_College_GeoGuesser_AS91907_Lightfoot.py
+++ Howick_College_GeoGuesser_AS91907_Lightfoot.py
@@ -10,22 +10,31 @@ class Game:
         self.game = game
         self.guess = guess
 
-    def get_guess(self):
+    def get_guess(self, root):
         self.guess = guess.get()
         answer = answer.get()
         if self.guess == answer:
             self.points + 1
             self.game + 1
-            correct = Label(root, text="Correct!", fg='lime', font=("Calibri", "15", "bold")).grid(column=3, row=4)
-            return self.points, self.game
+            try:
+                incorrect.destroy()
+            except NameError:
+                correct = Label(root, text="Correct!", fg='lime', font=("Calibri", "15", "bold")).place(x=770, y=900)
         else:
-            correct.destroy()
-            incorrect = Label(root, text="Incorrect!", fg='red', font=("Calibri", "15", "bold")).grid(column=3, row=4)
-            time.sleep(3)
-            incorrect.destroy()
+            try:
+                correct.destroy()
+            except NameError:
+                incorrect = Label(root, text="Incorrect!", fg='red', font=("Calibri", "15", "bold")).place(x=770, y=900)
+        return self.points, self.game
         
 def quit(root):
     root.destroy()
+    
+    def reset(self, root):
+        self.points = 0
+        self.game = 1
+        self.time = 0
+        return self.points, self.game, self.time
                
 # G.U.I
 root = Tk()
@@ -38,17 +47,19 @@ guess_text_box = Entry(root, textvariable=guess, width=34, font=("Calibiri", "11
 guess_text_box_label = Label(root, text="Location:", fg="black", bg="cyan",
                                 font=("Calibri", "15", "bold")).place(x=750, y=910)
 submit_btn = Button(root, text="Guess", borderwidth="5", width="13", bg='lime', fg='black',
-                         font=("Calibri", "15", "bold"),command=lambda:get_guess()).place(x=1050, y=900)
+                         font=("Calibri", "15", "bold"),command=lambda:get_guess(self, root)).place(x=1050, y=900)
 reset_btn = Button(root, text="Reset", borderwidth="5", width="13", bg='lime', fg='black',
-                         font=("Calibri", "15", "bold"),command=lambda:get_guess()).place(x=600, y=900)
+                         font=("Calibri", "15", "bold"),command=lambda:reset(self, root)).place(x=600, y=900)
 quit_btn = Button(root, text="Quit", borderwidth="5", width="13", bg='lime', fg='black',
                          font=("Calibri", "15", "bold"),command=lambda:quit(root)).place(x=450, y=900)
 
-how_to_play_frame = Frame(root, bg='lime', width=430, height=725, highlightbackground="black", highlightthickness=5).place(x=1480, y=168)
-how_to_play_title = Label(root, bg='lime', text="How to play?", font=("Calibri", "18", "bold")).place(x=1490, y=175)
+how_to_play_frame = Frame(root, bg='cyan', width=430, height=714, highlightbackground="black", highlightthickness=5).place(x=1480, y=173)
+how_to_play_title = Label(root, fg="darkblue", bg='cyan', text="How to play?", font=("Calibri", "20", "bold", "underline")).place(x=1490, y=178)
+how_to_play_text = Label(root, bg='cyan', text="- Type your guess in the text box 'location' undeneath the image.\n\n Ensure the use of correct grammer and clear guesses.\n\n- Gather as many points as possible and try and get your best time!", 
+                         font=("Calibri", "11", "bold")).place(x=1490, y=215)
 title = Label(root, text="Howick College GeoGuesser", bg='cyan', fg='darkblue',
-                         font=("Calibri", "30", "bold", "underline"),).place(x=740, y=100)
+                         font=("Calibri", "30", "bold", "underline")).place(x=740, y=100)
 location = PhotoImage(file="location.png")
 location_label = Label(root, height=700, width=1000, image=location, bg='white', highlightbackground="black",
-highlightthickness=5).place(anchor='center', x=960, y=530)
+                       highlightthickness=5).place(anchor='center', x=960, y=530)
 root.mainloop()
